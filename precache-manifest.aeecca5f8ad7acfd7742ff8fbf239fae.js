fb5ef72b946ed4c39b3e285e324547326fb26204
Pano Papadatos <pano.papadatos@publicrelay.com>
2020-02-06 18:41:49 -0500

    Updates

---
+++ precache-manifest.aeecca5f8ad7acfd7742ff8fbf239fae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfdbf33c19ffc8d77119c6c5ebc2c2be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2fb13b262e79011ca99",
+    "url": "/static/css/main.94f472f8.chunk.css"
+  },
+  {
+    "revision": "a0bfbaa1debeb5a5ed44",
+    "url": "/static/js/2.d85f445b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.d85f445b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2fb13b262e79011ca99",
+    "url": "/static/js/main.ea8a035e.chunk.js"
+  },
+  {
+    "revision": "f189a3821cc414c1a911",
+    "url": "/static/js/runtime-main.978be08d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
