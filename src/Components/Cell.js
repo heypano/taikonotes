757adf88bb0bd560183fc4b7c6db491383cae211
Pano Papadatos <pano.papadatos@publicrelay.com>
2020-03-23 21:43:15 -0400

    random and progressive colors test

--- src/Components/Cell.js
+++ src/Components/Cell.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 
+const randomColors = true;
+const proggressiveColor = true;
+let lastC1 = getRandomColorNumber();
+let lastC2 = getRandomColorNumber();
+let lastC3 = getRandomColorNumber();
+
 const Cell = ({ index, divideEvery, cellsPerLine }) => {
   const [clicked, setClicked] = useState(false);
+  const [bg, setBg] = useState("transparent");
   let unclickedClass = "hover:bg-gray-300";
   let clickedClass = "bg-gray-900 hover:bg-gray-600 selected";
   if (index % divideEvery === 0) {
@@ -13,12 +20,44 @@ const Cell = ({ index, divideEvery, cellsPerLine }) => {
       className={`border border-blue-800 h-10 cursor-pointer ${
         clicked ? clickedClass : unclickedClass
       }`}
+      style={{
+        backgroundColor: bg
+      }}
       onClick={() => {
-        console.log(clicked);
+        if (randomColors) {
+          setBg(getRandomColor());
+        }
+        if (proggressiveColor) {
+          setBg(getProgressiveColor());
+        }
         setClicked(!clicked);
       }}
     ></div>
   );
 };
+
+function getProgressiveColor() {
+  const which = Math.floor(Math.random() * Math.floor(3));
+  const c1 = which === 0 ? getNext(lastC1) : lastC1;
+  const c2 = which === 1 ? getNext(lastC2) : lastC2;
+  const c3 = which === 2 ? getNext(lastC3) : lastC3;
+  return `rgb(${c1},${c2},${c3})`;
+}
+
+function getRandomColor() {
+  const c1 = getRandomColorNumber();
+  const c2 = getRandomColorNumber();
+  const c3 = getRandomColorNumber();
+  return `rgb(${c1},${c2},${c3})`;
+}
+
+function getRandomColorNumber() {
+  return Math.floor(Math.random() * Math.floor(256));
+}
+
+function getNext(num) {
+  const result = num + Math.floor(getRandomColorNumber());
+  return result % 256;
+}
 
 export default Cell;
--- src/Components/TaikoGrid.js
+++ src/Components/TaikoGrid.js
@@ -1,66 +1,100 @@
-import React, { useState } from "react";
+import React from "react";
 import Cell from "./Cell";
 import TaikoGridSettings from "./TaikoGridSettings";
 
 /**
  * React class to Show a Taiko Grid
  */
-const TaikoGrid = props => {
-  const cells = [];
-  const initialSettings = {
-    cellsPerLine: "16",
-    divideEvery: "4",
-    totalLines: "4"
-  };
-  const [settings, setSettings] = useState(initialSettings);
-  const numCells = settings.cellsPerLine * settings.totalLines;
-  const addLine = () => {
-    setSettings({
-      ...settings,
-      totalLines: +settings.totalLines + 1 + ""
-    });
-  };
-  const removeLine = () => {
-    setSettings({
-      ...settings,
-      totalLines: +settings.totalLines - 1 + ""
-    });
-  };
-  for (let i = 0; i < numCells; i++) {
-    cells.push(
-      <Cell
-        key={i}
-        index={i}
-        divideEvery={settings.divideEvery}
-        cellsPerLine={settings.cellsPerLine}
-      ></Cell>
-    );
+class TaikoGrid extends React.Component {
+  constructor(props) {
+    super(props);
+    const initialSettings = {
+      cellsPerLine: "16",
+      divideEvery: "4",
+      totalLines: "4"
+    };
+    this.state = {
+      settings: initialSettings
+    };
+    this.addLine = this.addLine.bind(this);
+    this.removeLine = this.removeLine.bind(this);
+    this.setSettings = this.setSettings.bind(this);
   }
 
-  return (
-    <div>
-      <div className="container settings mt-3 mb-3 p-3 flex flex-row">
-        <img
-          src={`${process.env.PUBLIC_URL}/favicon/taiko_sakura.svg`}
-          className="w-1/12"
-        />
-        <TaikoGridSettings settings={settings} setSettings={setSettings} />
+  /**
+   * Set the settings (passed as prop to children)
+   * @param settings
+   */
+  setSettings(settings) {
+    this.setState({
+      ...this.state,
+      settings: settings
+    });
+  }
+
+  /**
+   * Add a line to the grid
+   */
+  addLine() {
+    this.setSettings({
+      ...this.state.settings,
+      totalLines: +this.state.settings.totalLines + 1 + ""
+    });
+  }
+
+  /**
+   * Remove a line from the grid
+   */
+  removeLine() {
+    this.setSettings({
+      ...this.state.settings,
+      totalLines: +this.state.settings.totalLines - 1 + ""
+    });
+  }
+
+  render() {
+    const cells = [];
+    const { cellsPerLine, totalLines, divideEvery } = this.state.settings;
+    const numCells = cellsPerLine * totalLines;
+    for (let i = 0; i < numCells; i++) {
+      cells.push(
+        <Cell
+          key={i}
+          index={i}
+          divideEvery={divideEvery}
+          cellsPerLine={cellsPerLine}
+        ></Cell>
+      );
+    }
+
+    return (
+      <div>
+        <div className="container settings mt-3 mb-3 p-3 flex flex-row">
+          <img
+            src={`${process.env.PUBLIC_URL}/favicon/taiko_sakura.svg`}
+            className="w-1/12"
+          />
+          <TaikoGridSettings
+            settings={this.state.settings}
+            setSettings={this.setSettings}
+          />
+        </div>
+        <div
+          className={`grid grid-cols-${cellsPerLine} border border-blue-800`}
+        >
+          {cells}
+        </div>
+        <div className="flex flex-row justify-between">
+          <a className="text-5xl cursor-pointer" onClick={this.removeLine}>
+            ➖
+          </a>
+          <a className="text-5xl cursor-pointer" onClick={this.addLine}>
+            ➕
+          </a>
+        </div>
       </div>
-      <div
-        className={`grid grid-cols-${settings.cellsPerLine} border border-blue-800`}
-      >
-        {cells}
-      </div>
-      <div className="flex flex-row justify-between">
-        <a className="text-5xl cursor-pointer" onClick={removeLine}>
-          ➖
-        </a>
-        <a className="text-5xl cursor-pointer" onClick={addLine}>
-          ➕
-        </a>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default TaikoGrid;
