fdb92b3e06345268d111d3c4bf10d729a221b8f4
Pano Papadatos <pano.papadatos@publicrelay.com>
2020-04-30 22:07:41 -0400

    making Cell a class component

--- src/Components/Cell.js
+++ src/Components/Cell.js
@@ -1,50 +1,64 @@
-import React, { useState } from "react";
+import React from "react";
+import * as PropTypes from "prop-types";
 
-/**
- * Return the next sound
- * @param {[]} sounds
- * @param {number} lastSelected
- * @returns {number}
- */
-const getNextSound = (sounds, lastSelected) => {
-  if (lastSelected == null) {
-    return 0;
-  } else if (lastSelected + 1 < sounds.length) {
-    return lastSelected + 1;
-  } else {
-    return null;
+class Cell extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedSoundIndex: null
+    };
   }
-};
-
-const Cell = ({ index, divideEvery, cellsPerLine, sounds }) => {
-  const [selectedSoundIndex, setSelectedSoundIndex] = useState(null);
-  const [bg, setBg] = useState(null);
-  let backgroundClass = "hover:bg-gray-300";
-  if (index % divideEvery === 0) {
-    backgroundClass = "bg-gray-300 hover:bg-gray-600";
+  /**
+   * Return the next sound
+   * @param {[]} sounds
+   * @param {number} lastSelected
+   * @returns {number}
+   */
+  getNextSound(sounds, lastSelected) {
+    if (lastSelected == null) {
+      return 0;
+    } else if (lastSelected + 1 < sounds.length) {
+      return lastSelected + 1;
+    } else {
+      return null;
+    }
   }
-  const styleObject = bg
-    ? {
-        backgroundColor: bg
-      }
-    : {};
 
-  return (
-    <div
-      className={`flex flex-row justify-center items-center select-none border border-blue-800 h-10 cursor-pointer ${backgroundClass}`}
-      style={styleObject}
-      onClick={() => {
-        const nextSound = getNextSound(sounds, selectedSoundIndex);
-        setSelectedSoundIndex(nextSound);
-      }}
-      onContextMenu={e => {
-        e.preventDefault();
-        setSelectedSoundIndex(null);
-      }}
-    >
-      {sounds[selectedSoundIndex]}
-    </div>
-  );
+  getCurrentSound;
+
+  render() {
+    let { index, divideEvery, sounds } = this.props;
+    let backgroundClass = "hover:bg-gray-300";
+    if (index % divideEvery === 0) {
+      backgroundClass = "bg-gray-300 hover:bg-gray-600";
+    }
+
+    return (
+      <div
+        className={`flex flex-row justify-center items-center select-none border border-blue-800 h-10 cursor-pointer ${backgroundClass}`}
+        onClick={() => {
+          const nextSound = this.getNextSound(
+            sounds,
+            this.state.selectedSoundIndex
+          );
+          this.setState({ ...this.state, selectedSoundIndex: nextSound });
+        }}
+        onContextMenu={e => {
+          e.preventDefault();
+          this.setState({ ...this.state, selectedSoundIndex: null });
+        }}
+      >
+        {sounds[this.state.selectedSoundIndex]}
+      </div>
+    );
+  }
+}
+
+Cell.propTypes = {
+  index: PropTypes.any,
+  divideEvery: PropTypes.any,
+  cellsPerLine: PropTypes.any,
+  sounds: PropTypes.any
 };
 
 export default Cell;
