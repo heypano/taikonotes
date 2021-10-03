d4d858222173c82222f64d9372e4df3c06033ad2
Pano Papadatos <pano@heypano.com>
2021-10-03 12:29:41 -0400

    Adding mongodb start

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import clientPromise from "../../lib/mongodb";
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+
+  const movies = await client
+    .db("taikonotes")
+    .collection("songs")
+    .find({})
+    .toArray();
+  res.json(movies);
 }
