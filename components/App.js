98469e34d0f72af16645f2961c8c10908f1351f4
Pano Papadatos <pano@heypano.com>
2022-01-29 21:25:36 +0200

    Added onbeforeunload handler

--- components/App.js
+++ components/App.js
@@ -1,9 +1,22 @@
 import { Provider } from "react-redux";
 import PropTypes from "prop-types";
+import { useEffect } from "react";
 import { store } from "../redux/store";
 import Main from "./Main";
 
+const beforeUnloadHandler = (event) => {
+  const question = "Are you sure you want to exit?";
+  event.preventDefault();
+  // eslint-disable-next-line no-param-reassign,no-return-assign
+  return (event.returnValue = question);
+};
+
 function App({ song, error }) {
+  useEffect(() => {
+    window.addEventListener("beforeunload", beforeUnloadHandler, {
+      capture: true,
+    });
+  }, []);
   return (
     <Provider store={store}>
       <Main song={song} error={error} />
